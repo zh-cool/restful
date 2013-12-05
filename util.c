bb8bf8d8e0a18f34be71202137c38152964acdf8
austin <austin@austin.(none)>
2013-12-05 16:27:36 +0800

    add util

---
+++ util.c
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <net/if.h> 
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <error.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include "util.h"
+
+int cov2xml(char *dst, const char *src, int len)
+{
+	char *ptr = dst;
+	int n = 0, sum = 0;
+
+	while(len--){
+		switch(*src){
+			case '<':
+				n=sprintf(ptr, "%s", "&lt;");
+				break;
+			case '>':
+				n=sprintf(ptr, "%s", "&gt;");
+				break;
+			case '&':
+				n=sprintf(ptr, "%s", "&apos;");
+				break;
+			case '\'':
+				n=sprintf(ptr, "%s", "&apos;");
+				break;
+			case '"':
+				n=sprintf(ptr, "%s", "&quot;");
+				break;
+			default:
+				n = 1;
+				*ptr = *src;
+		}
+		ptr += n;
+		src++;
+		sum += n;
+	}
+	return sum;
+}
+
+char* get_wan_ip(char *ip, int len)
+{
+	FILE *fp = popen(". /lib/functions/network.sh; network_get_ipaddr ip wan; echo $ip", "r");
+	fgets(ip, len, fp);
+	pclose(fp);
+	return ip;	
+}
+
+char* get_wan_gateway(char *gateway, int len)
+{
+	FILE *fp = popen(". /lib/functions/network.sh; network_get_gateway ip wan; echo $ip", "r");
+	fgets(gateway, len, fp);
+	pclose(fp);
+	return gateway;	
+}
+
+char* get_wan_dns(char *dns, int len)
+{
+	FILE *fp = popen(". /lib/functions/network.sh; network_get_dnsserver ip wan; echo $ip", "r");
+	fgets(dns, len, fp);
+	pclose(fp);
+	return dns;
+}
+
+int isip(const char *ip)
+{
+	struct in_addr in;
+	return inet_pton(AF_INET, ip, &in);
+}
+
+int isnetmask(const char *netmask)
+{
+	struct in_addr in;
+	if(!inet_pton(AF_INET, netmask, &in)){
+		return 0;
+	}
+
+	uint32_t mask = in.s_addr;
+	mask = ntohl(mask);
+	return ((mask|(mask-1))==0xFFFFFFFF);
+}
+
+int uci_set_cfg(const char *csp, const char *value)
+{
+	int len = 0;
+	char *cfg = 0;
+	len = strlen(csp)+strlen(value)+16;
+
+       	cfg = malloc(len);
+	len = snprintf(cfg, len, "uci set %s='%s'", csp, value);
+	system(cfg);
+	
+	free(cfg);
+	return 0;
+}
+
+int uci_commit_change(const char *config)
+{
+	char cmd[128] = {0};
+	snprintf(cmd, sizeof(cmd), "uci commit %s", config);
+	system(cmd);
+	return 0;
+}
+
+char* uci_get_cfg(const char *cfg, char *buf, int len)
+{
+	FILE *fp = NULL;
+	char cmd[512];
+
+	buf[0] = 0;
+	snprintf(cmd, sizeof(cmd), "uci get %s", cfg);
+	fp = popen(cmd, "r");
+	printf("uci get %s\n", cfg);
+	if(NULL == fp){
+		return buf;
+	}
+
+	fgets(buf, len, fp);
+
+	if(pclose(fp) < 0){
+		return buf;
+	}
+
+	len = strlen(buf);
+	if(len && isspace(buf[len-1])){
+		buf[len-1] = 0;
+	}
+
+	return buf;
+}
+
+int get_interface_cfg(const char *ifname, struct if_cfg *cfg)
+{
+	struct ifreq ifr;
+	struct sockaddr_in* ipaddr = NULL;
+	int fd = 0, len=0;
+
+	if((fd=socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		return errno;
+	}
+
+	strcpy(ifr.ifr_name, ifname);
+	if (ioctl(fd, SIOCGIFHWADDR, &ifr) < 0){
+		return errno;
+	}
+	len=snprintf(cfg->mac, sizeof(cfg->mac), 
+			"%02X:%02X:%02X:%02X:%02X:%02X", 
+			(uint8_t)ifr.ifr_hwaddr.sa_data[0],
+			(uint8_t)ifr.ifr_hwaddr.sa_data[1],
+			(uint8_t)ifr.ifr_hwaddr.sa_data[2],
+			(uint8_t)ifr.ifr_hwaddr.sa_data[3],
+			(uint8_t)ifr.ifr_hwaddr.sa_data[4],
+			(uint8_t)ifr.ifr_hwaddr.sa_data[5]
+		);
+	cfg->mac[len] = 0;
+
+	if(ioctl(fd,SIOCGIFADDR,&ifr) < 0) {
+		return errno;
+	}
+	ipaddr = (struct sockaddr_in*)&ifr.ifr_addr;
+	inet_ntop(AF_INET, &ipaddr->sin_addr, cfg->ip, sizeof(struct sockaddr_in));
+
+	if(ioctl(fd, SIOCGIFNETMASK, &ifr) < 0){
+		return errno;
+	}
+	ipaddr = (struct sockaddr_in*)&ifr.ifr_netmask;
+	inet_ntop(AF_INET, &ipaddr->sin_addr, cfg->netmask, sizeof(struct sockaddr_in));
+
+	if(ioctl(fd, SIOCGIFBRDADDR, &ifr) < 0){
+		return errno;
+	}
+	ipaddr = (struct sockaddr_in*)&ifr.ifr_broadaddr;
+	inet_ntop(AF_INET, &ipaddr->sin_addr, cfg->broadaddr, sizeof(struct sockaddr_in));
+
+	close(fd);
+	return 0;	
+}
+
+struct arp_tbl* get_arp_tbl(void)
+{
+	FILE *fp = NULL;
+	char line[128];
+	struct arp_tbl tbl, *ptbl=&tbl, *ptr=NULL;
+	
+	fp = fopen("/proc/net/arp", "r");
+	fgets(line, sizeof(line), fp);
+
+	while(fgets(line, sizeof(line), fp)){
+		ptr = malloc(sizeof(struct arp_tbl));
+		ptr->next = NULL;
+		ptbl->next = ptr;
+		ptbl = ptr;
+		sscanf(line, "%s 0x%x 0x%x %s %s %s\n", ptbl->ip, &ptbl->type, &ptbl->flags, ptbl->hwa, ptbl->mask, ptbl->dev);
+	}
+	fclose(fp);
+
+	if(ptbl == &tbl){
+		return NULL;
+	}
+	return tbl.next;
+}
+
+void free_arp_tbl(struct arp_tbl *tbl)
+{
+	struct arp_tbl *ptr = NULL;
+
+	while(tbl->next){
+		ptr = tbl;
+		tbl = tbl->next;
+		free(ptr);
+	}
+	free(tbl);
+}
+
+char* find_ip_tbl(struct arp_tbl* tbl, char *max)
+{
+	while(tbl){
+		if(!strcasecmp(tbl->hwa, max)){
+			return tbl->ip;	
+		}
+		tbl = tbl->next;
+	}
+	return NULL;
+}
---
+++ util.h
@@ -0,0 +1,35 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+int cov2xml(char *dst, const char *src, int len);
+char* get_wan_ip(char *ip, int len);
+char* get_wan_dns(char *dns, int len);
+char* get_wan_gateway(char *gateway, int len);
+int isip(const char *ip);
+int isnetmask(const char *netmask);
+char* uci_get_cfg(const char *cfg, char *buf, int len);
+int uci_set_cfg(const char *csp, const char *value);
+int uci_commit_change();
+
+struct if_cfg{
+	char	mac[32];
+	char    ip[32];
+	char    netmask[32];
+	char    broadaddr[32];
+};
+int get_interface_cfg(const char *ifname, struct if_cfg *cfg);
+
+struct arp_tbl{
+	struct arp_tbl *next;
+	char ip[128];
+	int type;
+	int flags;
+	char hwa[128];
+	char mask[128];
+	char dev[128];
+};
+struct arp_tbl* get_arp_tbl(void);
+void free_arp_tbl(struct arp_tbl *tbl);
+char* find_ip_tbl(struct arp_tbl *tbl, char *max);
+
+#endif
